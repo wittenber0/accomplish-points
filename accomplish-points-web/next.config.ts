447d69d5d71b13b6070d1315ee08e43322652406
wittenber0 <ryanwwittenberg@gmail.com>
2026-05-15 14:04:21 -0400

    optimizer quality

--- accomplish-points-web/next.config.ts
+++ accomplish-points-web/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	images: {
+		qualities: [75, 95],
+	},
+};
 
 export default nextConfig;
